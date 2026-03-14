d33b88d336b3408cce41e9bd2c1f3ec89058269d
Jyotiprakash Pal <jyotiprakash.pal@acxiomconsulting.com>
2026-03-14 09:26:53 +0530

    add the base url

--- app/services/api/baseurl.tsx
+++ app/services/api/baseurl.tsx
@@ -1,4 +1,5 @@
-export const BASE_URL = 'http://192.168.76.248:5000/api';
+// export const BASE_URL = 'http://192.168.76.248:5000/api';
+export const BASE_URL = 'http://192.168.77.52:5000/api'; //online ip address of the backend server
 
 // export const BASE_URL = 'http://localhost:5000/api';
 
